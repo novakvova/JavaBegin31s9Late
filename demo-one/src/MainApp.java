b4217e7bc77249e52eae3e7686a1a777a453f6b0
novakvova <novakvova@gmail.com>
2020-01-13 10:28:00 +0200

    Work array and try

--- demo-one/src/MainApp.java
+++ demo-one/src/MainApp.java
@@ -1,8 +1,28 @@
+import java.util.*;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// System.out.println("Привіт!");
+		try {
+			Scanner in = new Scanner(System.in);
+			int[] nums = new int[5];
+			for (int i = 0; i < nums.length; i++) {
+				System.out.printf("->_");
+				nums[i] = in.nextInt();
+			}
+			for (int i = 0; i < nums.length; i++) {
+				System.out.printf("%d\t", nums[i]);
+			}
+			in.close();
+		}
+		catch(InputMismatchException ex) {
+			System.out.println("Проблема вводу даних!");
+		}
+		catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
 
 	}
 
