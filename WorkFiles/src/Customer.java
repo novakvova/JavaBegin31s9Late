de6b63f1621ab9087d972d39fa7368adbd016787
novakvova <novakvova@gmail.com>
2020-01-22 11:50:05 +0200

    Work files

---
+++ WorkFiles/src/Customer.java
@@ -0,0 +1,18 @@
+
+public class Customer {
+	private String name;
+	private int age;
+
+	public Customer(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public int GetAge() {
+		return age;
+	}
+
+	public String GetName() {
+		return name;
+	}
+}
--- WorkFiles/src/MainApp.java
+++ WorkFiles/src/MainApp.java
@@ -1,3 +1,7 @@
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,8 +14,11 @@ public class MainApp {
 	public static void main(String[] args) {
 
 		// ReadFile();
-		 WriteFile();
+		// WriteFile();
 		// InfoFileShow();
+		// CreateFolder();
+		WriteUTF8();
+		ReadUTF8();
 	}
 
 	private static void ReadFile() {
@@ -38,7 +45,6 @@ public class MainApp {
 		}
 
 		try (FileInputStream fin = new FileInputStream(fileName)) {
-			
 			System.out.println("Byte file size: " + fin.available());
 			System.out.println("Show file");
 //			int item=-1;
@@ -60,10 +66,9 @@ public class MainApp {
 
 	private static void InfoFileShow() {
 		try {
-			 System.out.println("Working Directory = " +
-		              System.getProperty("D:\\test.txt"));
-			
-			Path file = Paths.get("D:\\test.txt");
+			System.out.println("Working Directory = " + System.getProperty("test.txt"));
+
+			Path file = Paths.get("test.txt");
 			BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
 
 			System.out.println("creationTime: " + attr.creationTime());
@@ -75,9 +80,67 @@ public class MainApp {
 			System.out.println("isRegularFile: " + attr.isRegularFile());
 			System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
 			System.out.println("size: " + attr.size());
-			
+
 		} catch (Exception ex) {
-			System.out.println("Error "+ex.getMessage());
+			System.out.println("Error " + ex.getMessage());
+		}
+	}
+
+	private static void CreateFolder() {
+		File dir = new File("step");
+		boolean isCreatDirectory = dir.mkdir();
+		if (isCreatDirectory) {
+			System.out.println("Ствоерня папки успішне!");
+		}
+		if (dir.exists()) {
+			System.out.println("Папка існує!");
+		}
+		File newDir = new File("ttt");
+		dir.renameTo(newDir);
+		// newDir.delete();
+		String par = newDir.getParent();
+		System.out.println(par);
+	}
+
+	private static void WriteUTF8() {
+		Customer[] list = new Customer[] { new Customer("Іван", 12), new Customer("Семен", 23),
+				new Customer("Оксана", 18), new Customer("Діма", 12), new Customer("Саша", 33) };
+		
+		try(DataOutputStream fileData = 
+				new DataOutputStream(
+						new FileOutputStream("customer2.bin", true)))
+		{
+			//fileData.writeInt(list.length);
+			for(Customer cust : list)
+			{
+				fileData.writeUTF(cust.GetName());
+				fileData.writeInt(cust.GetAge());
+			}
+			System.out.println("Добре.");
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	private static void ReadUTF8() {
+		try(DataInputStream fileData = 
+				new DataInputStream(
+						new FileInputStream("customer2.bin")))
+		{
+			//int size=fileData.readInt();
+			while(true)
+			{
+				String name = fileData.readUTF();
+				int age = fileData.readInt();
+				System.out.println(name+" "+age);
+			}
+			
+		}
+		catch(IOException ex)
+		{
+			System.out.println(ex.getMessage());
 		}
 	}
 }
